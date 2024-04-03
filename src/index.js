70553ac916cebe5ad573061997ac21dd7ba9c02e
galKatzav <galoosh1591@gmail.com>
2024-04-03 13:03:02 +0300

    NotFound screen & Routers & Modules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import HomePage from "./components/screens/HomePage";
+import AboutMe from "./components/screens/AboutMe";
+import AddDonation from "./components/screens/AddDonation";
+import ConnectUs from "./components/screens/ConnectUs";
+import SearchDonations from "./components/screens/SearchDonations";
+import LogIn from "./components/screens/LogIn";
+import Registertion from "./components/screens/Registertion";
+import NotFound from "./components/screens/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Route for Home page */}
+        <Route path="/About_Me" element={<AboutMe />} />
+        <Route path="/Add_Donation" element={<AddDonation />} />
+        <Route path="/Connect_Us" element={<ConnectUs />} />
+        <Route path="/Donations_Search" element={<SearchDonations />} />
+        <Route path="/Log_In" element={<LogIn />} />
+        <Route path="/Registertion" element={<Registertion />} />
+        <Route path="*" element={<NotFound />} />{" "}
+        {/* Route for handling 404 errors */}
+      </Routes>
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
