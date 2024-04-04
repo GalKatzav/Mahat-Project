3d68741b0573123069206326630cc5aa23945eb5
galKatzav <galoosh1591@gmail.com>
2024-04-04 15:58:50 +0300

    Reg page & LogIn CSS update

--- src/components/screens/Registertion.js
+++ src/components/screens/Registertion.js
@@ -1,5 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import "../screensCSS/Registertion.css";
+// import backroundReg from "../../images/backroundReg.jpg";
 
-export default function Registertion() {
-  return <div>Registertion</div>;
-}
+const Registertion = () => {
+  const [form, setForm] = useState({
+    fullName: "",
+    userName: "",
+    password: "",
+    email: "",
+    address: "",
+    phone: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle the registration logic here
+    console.log(form);
+  };
+
+  return (
+    <div className="registration-container">
+      {/* <img src={backroundReg} alt="backroundReg"></img> */}
+      <form className="registration-form" onSubmit={handleSubmit}>
+        <h2>Create New User</h2>
+        <div className="input-group">
+          <input
+            type="text"
+            name="fullName"
+            value={form.fullName}
+            onChange={handleChange}
+            placeholder="Full Name"
+            required
+          />
+          <input
+            type="text"
+            name="address"
+            value={form.address}
+            onChange={handleChange}
+            placeholder="Geographic Address"
+            required
+          />
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            name="userName"
+            value={form.userName}
+            onChange={handleChange}
+            placeholder="User Name"
+            required
+          />
+          <input
+            type="text"
+            name="phone"
+            value={form.phone}
+            onChange={handleChange}
+            placeholder="Phone Number"
+            required
+          />
+        </div>
+        <div className="input-group">
+          <input
+            type="password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            placeholder="Password"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Email Address"
+            required
+          />
+        </div>
+        <button type="submit" className="save-button">
+          SAVE
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Registertion;
