a037b50c1a8024715933cf56399a7352922a8e75
galKatzav <galoosh1591@gmail.com>
2024-04-10 14:41:45 +0300

    Reg & Home Pages CSS

--- src/components/screens/HomePage.js
+++ src/components/screens/HomePage.js
@@ -4,21 +4,23 @@ import "../screensCSS/HomePage.css"; // Make sure the CSS file path is correct
 
 const HomePage = () => {
   return (
-    <div className="main-content">
-      <h1>EMPOWER CHANGE, DONATE TODAY: TOGETHER WE WILL WIN!</h1>
-      <div className="content-container">
-        <div className="stat">
-          <p>450,000</p>
-          <p>משפחות מקבלות שירות</p>
-        </div>
-        <div className="call-to-action">
-          <Link to="/Log_In">
-            <button>לתרומה מהירה לחץ כאן</button>
-          </Link>
-        </div>
-        <div className="stat">
-          <p>160,000</p>
-          <p>תרומות שנאספו</p>
+    <div className="HomePage">
+      <div className="main-content">
+        <h1>EMPOWER CHANGE, DONATE TODAY: TOGETHER WE WILL WIN!</h1>
+        <div className="content-container">
+          <div className="stat">
+            <p>450,000</p>
+            <p>Families receiving service</p>
+          </div>
+          <div className="call-to-action">
+            <Link to="/Log_In">
+              <button>For immediate donations, please click here.</button>
+            </Link>
+          </div>
+          <div className="stat">
+            <p>160,000</p>
+            <p>Donations collected</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/screens/Registertion.js
+++ src/components/screens/Registertion.js
@@ -53,67 +53,69 @@ const Registration = () => {
   };
 
   return (
-    <div className="registration-container">
-      <form className="registration-form" onSubmit={handleSubmit}>
-        <h2>Create New User</h2>
-        <div className="input-group">
-          <input
-            type="text"
-            name="fullName"
-            value={form.fullName}
-            onChange={handleChange}
-            placeholder="Full Name"
-            required
-          />
-          <input
-            type="text"
-            name="address"
-            value={form.address}
-            onChange={handleChange}
-            placeholder="Geographic Address"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <input
-            type="text"
-            name="userName"
-            value={form.userName}
-            onChange={handleChange}
-            placeholder="User Name"
-            required
-          />
-          <input
-            type="text"
-            name="phone"
-            value={form.phone}
-            onChange={handleChange}
-            placeholder="Phone Number"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <input
-            type="password"
-            name="password"
-            value={form.password}
-            onChange={handleChange}
-            placeholder="Password"
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-            placeholder="Email Address"
-            required
-          />
-        </div>
-        <button type="submit" className="save-button">
-          ADD NEW USER
-        </button>
-      </form>
+    <div className="Registration">
+      <div className="registration-container">
+        <form className="registration-form" onSubmit={handleSubmit}>
+          <h2>Create New User</h2>
+          <div className="input-group">
+            <input
+              type="text"
+              name="fullName"
+              value={form.fullName}
+              onChange={handleChange}
+              placeholder="Full Name"
+              required
+            />
+            <input
+              type="text"
+              name="address"
+              value={form.address}
+              onChange={handleChange}
+              placeholder="Geographic Address"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="text"
+              name="userName"
+              value={form.userName}
+              onChange={handleChange}
+              placeholder="User Name"
+              required
+            />
+            <input
+              type="text"
+              name="phone"
+              value={form.phone}
+              onChange={handleChange}
+              placeholder="Phone Number"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          <button type="submit" className="save-button">
+            ADD NEW USER
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
