305f3637d48b3aa66bbdec1eaaed2f1823d20b12
galKatzav <galoosh1591@gmail.com>
2024-04-18 12:37:35 +0300

    AboutMe Update

--- src/components/screens/AboutMe.js
+++ src/components/screens/AboutMe.js
@@ -54,28 +54,25 @@ const AboutMe = () => {
       <div className="about-me-content">
         {/* Content */}
         <h2>About Me</h2>
-        <p>Hi, I'm glad you're here.</p>
+        <p>Hi, We are glad you're here.</p>
         <p>
-          I'm part of a team that volunteers at a local community center. Every
-          week we gather and organize food packages for over 160,000 families
-          who do not have enough income to afford it. The happiness in the
-          children's eyes when they receive the packages gives us the motivation
-          to continue and expand the volunteering circles.
+          We are a non-profit organization working to break the cycle of poverty
+          in Israrl. The organization helps about 160,000 families every year
+          regardless of religion, race, origin or nationally. The organization
+          maintains direct contact with the recipoents of the aid, which allows
+          solutions to be adapted to immediate and unique needs that arise from
+          the field.
         </p>
         <p>
-          The volunteering team includes high school students, soldiers, and
-          other good-hearted people. We sort and pack the products, which
-          include fruits, vegetables, dry goods, and dairy, and distribute the
-          packages to the families, elderly, and anyone else in need.
+          Our vision is to break the cycle of intergenerational poverty in
+          Israel in order to realize our vision, we help thousands of needy
+          families, who are sent to us with referral letters from social
+          workers, all over the country. And we distribute thousands of food
+          baskets to families and babies, clothing, furniture, legal and medical
+          assistance and more.
         </p>
-        <p>
-          Thanks to our volunteers' dedication, we manage to bring a smile to
-          the faces of thousands each week.
-        </p>
-        <p>
-          For full details and to join our volunteering team, contact us at 123
-          Main Street.
-        </p>
+        <p>our address 123 Chazon Ish St. Tel Aviv</p>
+        <h3>Together we will win 🙏💕</h3>
       </div>
     </div>
   );
