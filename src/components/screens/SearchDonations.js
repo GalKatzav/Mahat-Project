c4955879f9545f80562ec5e46b0827b61426c0fd
galKatzav <galoosh1591@gmail.com>
2024-06-06 11:12:18 +0300

    Organization Info Update

--- src/components/screens/SearchDonations.js
+++ src/components/screens/SearchDonations.js
@@ -32,33 +32,34 @@ function SearchDonations() {
 
   return (
     <div className="container">
-      {selectedAssoc ? (
-        <div
-          className="org-details centered"
-          onClick={() => toggleOpen(selectedAssoc)}
-        >
-          <h1>{selectedAssoc.nameAssociations}</h1>
-          <p>Information: {selectedAssoc.information}</p>
-          <p>Donation Requests: {selectedAssoc.donationRequest.join(", ")}</p>
-          <p>District: {selectedAssoc.district}</p>
-          <p>Address: {selectedAssoc.addressAssociations}</p>
-          <p>Phone: {selectedAssoc.phoneAssociations}</p>
-        </div>
-      ) : (
-        <div className="org-list">
-          {associations.map((assoc) => (
-            <div
-              key={assoc.id}
-              className="org-entry"
-              onClick={() => toggleOpen(assoc)}
-            >
-              <h1>{assoc.nameAssociations}</h1>
-              <p>District: {assoc.district}</p>
-              <p>Donation Requests: {assoc.donationRequest.join(", ")}</p>
-            </div>
-          ))}
+      {selectedAssoc && (
+        <div className="overlay" onClick={() => toggleOpen(selectedAssoc)}>
+          <div
+            className="org-details centered"
+            onClick={() => toggleOpen(selectedAssoc)}
+          >
+            <h1>{selectedAssoc.nameAssociations}</h1>
+            <p>Information: {selectedAssoc.information}</p>
+            <p>Donation Requests: {selectedAssoc.donationRequest.join(", ")}</p>
+            <p>District: {selectedAssoc.district}</p>
+            <p>Address: {selectedAssoc.addressAssociations}</p>
+            <p>Phone: {selectedAssoc.phoneAssociations}</p>
+          </div>
         </div>
       )}
+      <div className="org-list">
+        {associations.map((assoc) => (
+          <div
+            key={assoc.id}
+            className="org-entry"
+            onClick={() => toggleOpen(assoc)}
+          >
+            <h1>{assoc.nameAssociations}</h1>
+            <p>District: {assoc.district}</p>
+            <p>Donation Requests: {assoc.donationRequest.join(", ")}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
