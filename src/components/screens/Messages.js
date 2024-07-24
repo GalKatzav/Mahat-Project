7679babdf7f5752e810af036fb9cf07ef7fbe405
galKatzav <galoosh1591@gmail.com>
2024-07-24 15:23:22 +0300

    Messages Update

--- src/components/screens/Messages.js
+++ src/components/screens/Messages.js
@@ -1,29 +1,99 @@
 import React, { useEffect, useState } from "react";
 import { firebase } from "../../services/firebase/FireStore";
 import {
+  getDocs,
   updateDoc,
   doc,
   onSnapshot,
+  getFirestore,
   query,
   collection,
   where,
+  addDoc,
 } from "firebase/firestore";
 import "../screensCSS/Messages.css";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useUser } from "../../services/contexts/UserContext";
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
   const [unreadCount, setUnreadCount] = useState(0);
   const [selectedMessage, setSelectedMessage] = useState(null);
-  const { user } = useUser();
+  const [showSendMessageForm, setShowSendMessageForm] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [newMessage, setNewMessage] = useState({
+    to: "",
+    subject: "",
+    message: "",
+  });
+  const [isAdmin, setIsAdmin] = useState(false);
+  const { user: currentUser } = useUser();
 
   useEffect(() => {
-    if (user && user.docId) {
+    console.log("useEffect called with currentUser:", currentUser);
+
+    const checkAdmin = async () => {
+      if (!currentUser) {
+        console.log("No currentUser, returning");
+        return;
+      }
+      console.log("Checking admin status for user:", currentUser);
+
+      const db = getFirestore();
+      const usersRef = collection(db, "users");
       const q = query(
-        collection(firebase, "messages"),
-        where("receiverId", "==", user.docId)
+        usersRef,
+        where("userName", "==", "admin"),
+        where("fullName", "==", "admin")
       );
 
+      try {
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const adminDoc = querySnapshot.docs[0];
+          console.log("Admin document ID:", adminDoc.id);
+
+          // Check if the current user's document ID matches the admin document ID
+          if (adminDoc.id === currentUser.id) {
+            console.log("Admin user found!");
+            setIsAdmin(true);
+          } else {
+            console.log("Current user is not admin.");
+          }
+        } else {
+          console.log("Admin user not found.");
+        }
+      } catch (error) {
+        console.error("Error checking admin status:", error);
+      }
+    };
+
+    if (currentUser) {
+      checkAdmin();
+    }
+  }, [currentUser]);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      if (!currentUser || !currentUser.uid) {
+        console.log("No currentUser or currentUser.uid, returning");
+        return;
+      }
+
+      console.log("Fetching messages for currentUser:", currentUser);
+
+      // Fetch messages from local storage
+      const localMessages = JSON.parse(localStorage.getItem("messages"));
+      if (localMessages) {
+        setMessages(localMessages);
+        setUnreadCount(localMessages.filter((message) => !message.read).length);
+      }
+
+      const q = query(
+        collection(firebase, "messages"),
+        where("receiverId", "==", currentUser.uid)
+      );
       const unsubscribe = onSnapshot(q, (snapshot) => {
         const userMessages = snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -38,8 +108,24 @@ const Messages = () => {
 
       // Cleanup listener on unmount
       return () => unsubscribe();
+    };
+
+    const fetchUsers = async () => {
+      const querySnapshot = await getDocs(collection(firebase, "users"));
+      const usersList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setUsers(usersList);
+    };
+
+    // Check if currentUser is defined in localStorage
+    const localCurrentUser = JSON.parse(localStorage.getItem("user"));
+    if (localCurrentUser) {
+      fetchMessages();
+      fetchUsers();
     }
-  }, [user, user.docId]);
+  }, [currentUser]);
 
   const markAsRead = async (messageId) => {
     const messageDoc = doc(firebase, "messages", messageId);
@@ -82,14 +168,60 @@ const Messages = () => {
     setSelectedMessage(selectedMessage ? null : message);
   };
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewMessage((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    const receiver = users.find((u) => u.id === newMessage.to);
+    if (!receiver) {
+      toast.error("User not found!");
+      return;
+    }
+    try {
+      await addDoc(collection(firebase, "messages"), {
+        subject: newMessage.subject,
+        message: newMessage.message,
+        receiverId: newMessage.to,
+        senderId: currentUser.uid,
+        read: false,
+        id: Date.now(),
+      });
+      toast.success("Message sent successfully!");
+      setShowSendMessageForm(false);
+      setNewMessage({
+        to: "",
+        subject: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error("Error sending message:", error);
+      toast.error("Failed to send message.");
+    }
+  };
+
   return (
     <div className="messages-container">
+      <ToastContainer />
       <div className="messages-header">
         <h1>Messages</h1>
         {unreadCount > 0 && (
           <div className="unread-count">
             <span>{unreadCount}</span>
           </div>
+        )}
+        {isAdmin && (
+          <button
+            className="btn btn-send-message"
+            onClick={() => setShowSendMessageForm(true)}
+          >
+            Send Message
+          </button>
         )}
       </div>
       <div className="messages-list-wrapper">
@@ -133,10 +265,57 @@ const Messages = () => {
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <h2>{selectedMessage.subject}</h2>
             <p>{selectedMessage.message}</p>
-            <button
-              className="btn btn-close"
-              onClick={() => toggleModal(null)}
-            ></button>
+            <button className="btn btn-close" onClick={() => toggleModal(null)}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      {showSendMessageForm && (
+        <div
+          className="form-overlay"
+          onClick={() => setShowSendMessageForm(false)}
+        >
+          <div className="form-container" onClick={(e) => e.stopPropagation()}>
+            <form onSubmit={handleSendMessage}>
+              <h2>Send Message</h2>
+              <label htmlFor="to">To:</label>
+              <select
+                name="to"
+                value={newMessage.to}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Select User</option>
+                {users.map((user) => (
+                  <option key={user.id} value={user.id}>
+                    {user.userName}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor="subject">Subject:</label>
+              <input
+                type="text"
+                name="subject"
+                value={newMessage.subject}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="message">Message:</label>
+              <textarea
+                name="message"
+                value={newMessage.message}
+                onChange={handleInputChange}
+                required
+              ></textarea>
+              <button type="submit">Send</button>
+              <button
+                type="button"
+                onClick={() => setShowSendMessageForm(false)}
+              >
+                Cancel
+              </button>
+            </form>
           </div>
         </div>
       )}
