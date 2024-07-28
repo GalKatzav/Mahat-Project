b1e6e98bb2145fdf81d75aadb049c6cbfdc68dd0
galKatzav <galoosh1591@gmail.com>
2024-07-28 11:44:52 +0300

    Setting File Update

--- src/components/screens/Settings.js
+++ src/components/screens/Settings.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from "react";
-import "../screensCSS/Settings.css"; // Update with your actual path to the CSS file
+import { useState, useEffect } from "react";
+import "../screensCSS/Settings.css"; // עדכני את הנתיב לקובץ ה-CSS
 import { firebase } from "../../services/firebase/FireStore";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { useUser } from "../../services/contexts/UserContext"; // Adjust the import path as necessary
+import { useUser } from "../../services/contexts/UserContext"; // עדכני את הנתיב לפי הצורך
+// import { getAuth } from "firebase/auth";
 
 function Settings() {
   const [profile, setProfile] = useState({
     fullName: "",
     email: "",
     password: "",
+    confirmPassword: "",
     phone: "",
     district: "",
     subscribe: false,
   });
 
-  const { user } = useUser(); // Get the current user's info
+  const { user } = useUser(); // קבלת המידע על המשתמש הנוכחי
   const [isAdmin, setIsAdmin] = useState(false);
+  // const auth = getAuth();
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -37,7 +40,7 @@ function Settings() {
           }));
 
           // Check if the user is an admin
-          if (userData.userName === "Admin" && userData.fullName === "Admin") {
+          if (userData.userName === "admin" && userData.fullName === "admin") {
             setIsAdmin(true);
           }
         } else {
@@ -59,29 +62,6 @@ function Settings() {
     }));
   };
 
-  const handlePasswordSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      if (!user.docId) {
-        console.error("No document ID found in user context");
-        return;
-      }
-      const userDocRef = doc(firebase, "users", user.docId); // Use document ID from context
-
-      // Update user document in Firebase
-      const updatedProfile = { password: profile.password };
-
-      console.log("Updating password for document ID:", user.docId); // Debug log
-      console.log("Updated password data:", updatedProfile); // Debug log
-
-      await updateDoc(userDocRef, updatedProfile);
-      alert("Password updated successfully!");
-    } catch (error) {
-      console.error("Error updating password:", error); // Debug log
-      alert("Failed to update password. Please try again.");
-    }
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -95,12 +75,16 @@ function Settings() {
       const updatedProfile = { ...profile };
       if (!profile.password) {
         delete updatedProfile.password; // Don't save the password field if it's empty
+        delete updatedProfile.confirmPassword; // Don't save the confirmPassword field if it's empty
+      } else {
+        updatedProfile.confirmPassword = profile.password;
       }
 
       console.log("Updating profile for document ID:", user.docId); // Debug log
       console.log("Updated profile data:", updatedProfile); // Debug log
 
       await updateDoc(userDocRef, updatedProfile);
+
       alert("Profile updated successfully!");
     } catch (error) {
       console.error("Error updating profile:", error); // Debug log
@@ -118,98 +102,88 @@ function Settings() {
 
   return (
     <div className="settings-page">
-      {isAdmin ? (
-        <form onSubmit={handlePasswordSubmit} className="settings-form">
-          <div className="settings-column">
-            <div className="form-control">
-              <label htmlFor="password">Password:</label>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                value={profile.password}
-                placeholder="Enter new password"
-                onChange={handleChange}
-              />
-            </div>
-            <button type="submit" className="save-button">
-              Save
-            </button>
+      <form onSubmit={handleSubmit} className="settings-form">
+        <div className="settings-column">
+          <div className="form-control">
+            <label htmlFor="fullName">Full Name:</label>
+            <input
+              id="fullName"
+              type="text"
+              name="fullName"
+              value={profile.fullName}
+              placeholder="Enter full name"
+              onChange={handleChange}
+            />
           </div>
-          <div className="spacer"></div> {/* Spacer div for margin */}
-        </form>
-      ) : (
-        <form onSubmit={handleSubmit} className="settings-form">
-          <div className="settings-column">
-            <div className="form-control">
-              <label htmlFor="fullName">Full Name:</label>
-              <input
-                id="fullName"
-                type="text"
-                name="fullName"
-                value={profile.fullName}
-                placeholder="Enter full name"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="email">Email Address:</label>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                value={profile.email}
-                placeholder="Enter email address"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="phone">Phone Number:</label>
-              <input
-                id="phone"
-                type="tel"
-                name="phone"
-                value={profile.phone}
-                placeholder="Enter phone number"
-                onChange={handleChange}
-              />
-            </div>
+          <div className="form-control">
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={profile.password}
+              placeholder="Enter new password"
+              onChange={handleChange}
+            />
           </div>
-          <div className="settings-column">
-            <div className="form-control">
-              <label htmlFor="password">Password:</label>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                value={profile.password}
-                placeholder="Enter new password"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="district">District:</label>
-              <select
-                id="district"
-                name="district"
-                value={profile.district}
-                onChange={handleChange}
-              >
-                <option value="">Select a district</option>
-                {districts.map((district) => (
-                  <option key={district} value={district}>
-                    {district}
-                  </option>
-                ))}
-              </select>
-            </div>
+          <div className="form-control">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              id="confirmPassword"
+              type="password"
+              name="confirmPassword"
+              value={profile.confirmPassword}
+              placeholder="Confirm new password"
+              onChange={handleChange}
+            />
           </div>
-          <button type="submit" className="save-button">
-            Save
-          </button>
-          <div className="spacer"></div> {/* Spacer div for margin */}
-        </form>
-      )}
+          <div className="form-control">
+            <label htmlFor="phone">Phone Number:</label>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              value={profile.phone}
+              placeholder="Enter phone number"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="settings-column">
+          <div className="form-control">
+            <label htmlFor="email">Email Address:</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value={profile.email}
+              placeholder="Enter email address"
+              disabled
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="district">District:</label>
+            <select
+              id="district"
+              name="district"
+              value={profile.district}
+              onChange={handleChange}
+            >
+              <option value="">Select a district</option>
+              {districts.map((district) => (
+                <option key={district} value={district}>
+                  {district}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <button type="submit" className="save-button">
+          Save
+        </button>
+        {isAdmin && <p>hey</p>}
+        <div className="spacer"></div> {/* Spacer div for margin */}
+      </form>
     </div>
   );
 }
